71c62e7a4a06bd584c9997279dbbe96c6df3b726
geniemo <kor98won@gmail.com>
2023-03-31 12:23:36 +0900

    FEAT: 페이지 라우팅
    - "/": 홈 경로
    - "/:id": 커뮤니티 상세

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import CommunityDetail from "./pages/CommunityDetail";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<CommunityDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
