1f9cdbe31b09e55a71b98443f6b4d8af6248f6d9
geniemo <kor98won@gmail.com>
2023-04-02 22:49:53 +0900

    EDIT: 카드 스타일 변경
    - 추후 API 완성 시 이미지 및 텍스트 변경 필요

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,15 +1,39 @@
 import styled from "styled-components";
 
 const StyledCard = styled.div`
-  width: 400px;
-  height: 200px;
+  width: 180px;
+  height: 240px;
+
   background-color: pink;
+
+  position: relative;
+
+  overflow: hidden;
+  border: 0px solid;
+  border-radius: 10px;
 `;
 
-const Card = ({ id }) => {
+const Card = () => {
   return (
     <StyledCard className="Card">
-      <h1>{id}</h1>
+      <img
+        src="https://cdn.lol.ps/assets/img/champion-loading/350.webp"
+        style={{ width: "100%", height: "100%", transform: "scale(1.1)" }}
+      />
+      <h3
+        style={{
+          width: "100%",
+          height: "fit-content",
+          textAlign: "center",
+          position: "absolute",
+          bottom: "0",
+          color: "white",
+          fontSize: "1rem",
+          background: "rgba(0, 0, 0, 0.7)",
+        }}
+      >
+        유미
+      </h3>
     </StyledCard>
   );
 };
